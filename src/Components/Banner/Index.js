1aee69058765dedb4cf05b1a53fd2b31bfe41f35
Carvalh0R0b3rt <carvalhorobert561@gmail.com>
2025-07-28 17:57:49 -0300

    Add images

---
+++ src/Components/Banner/Index.js
@@ -0,0 +1,18 @@
+import logo from './logo.png';
+import './App.css';
+
+
+function App() {
+    return (
+        <div className="App">
+            
+            <p>
+            <h1>Alura Rocks</h1>
+            </p>
+            
+        </div>
+    )
+
+}
+
+export default App;

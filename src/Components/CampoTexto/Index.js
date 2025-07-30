24ba19b1be68bae4013cfb993294f8fade66b7ea
Carvalh0R0b3rt <carvalhorobert561@gmail.com>
2025-07-30 17:29:14 -0300

    iniciando lista suspensa

--- src/Components/CampoTexto/Index.js
+++ src/Components/CampoTexto/Index.js
@@ -4,7 +4,7 @@ const CampoTexto = (props) => {
 
         //Para concatenar uma string em uso de props com placeholder e outras propriedades:
         //Javascript não é React! Abaixo estamos usando acentos graves ao invés de aspas simples ou duplas.
-           const placeholderModificada = `${props.placeholder}...`;
+  const placeholderModificada = `${props.placeholder}...`;
         //então basta usar a variável placeholderModificada no input.
         //outra forma de fazer isso é usar o operador lógico "||" para definir um valor padrão caso props.placeholder não seja fornecido; 
 

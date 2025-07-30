f38a6fe769bb6e0981f8f024653e51f3a62d5c63
Carvalh0R0b3rt <carvalhorobert561@gmail.com>
2025-07-30 15:29:37 -0300

    Adicionando estrutura para o formulário

--- src/Components/Formulario/index.js
+++ src/Components/Formulario/index.js
@@ -1,24 +1,23 @@
 import "./Formulario.css";
-import CampoTexto from "../CampoTexto/index";   // Importing CampoTexto component
+import CampoTexto from "../CampoTexto/index"; // Importing CampoTexto component
 
 const Formulario = () => {
-  
   //Não podemos esquecer de usar a tag return() dentro da função para garantir
   //que o componente seja exportado e renderizado corretamente pelo React.
   //Transferiemos todos os campos de texto do App.js para dentro de um formulário,
   //Assim podemos compartimentar e editar ele em um container adequado.
-  return(
-  <section className="formulario">
-    <form>
-      <h2>Preencha abaixo os dados do card do colaborador</h2>
-      <CampoTexto label="Nome" placeholder="Digite seu nome" />
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha abaixo os dados do card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
 
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
 
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem"/>
-    </form>
-  </section>
-  )
-}
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem"/>
+      </form>
+    </section>
+  );
+};
 
-export default Formulario
+export default Formulario;
